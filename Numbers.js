65379e60e49190cacc5c975eedef331a008d7e8b
Debraj Dey <35585013+deydebraj@users.noreply.github.com>
2019-03-18 22:11:51 -0400

    Create Numbers.js

---
+++ Numbers.js
@@ -0,0 +1,14 @@
+let value1 = 55;
+let value2 = "Hello World";
+let value3 = 'He can\'t do it';
+let value4 = "4";
+
+let value5 = -10/0;
+let value6 = 0/0;
+console.log(typeof(value1));
+console.log(typeof(value2));
+console.log(typeof(value3));
+console.log(value1+value4);
+console.log(value5);  //-Infinity
+console.log(value6);  //NaN
+console.log(typeof(value6));  //number
