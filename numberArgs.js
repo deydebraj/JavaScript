6a25b8340ced530a35b0cfe5f81bfdcfeadc1220
Debraj Dey <35585013+deydebraj@users.noreply.github.com>
2019-04-17 17:45:08 -0400

    Create numberArgs.js

---
+++ numberArgs.js
@@ -0,0 +1,8 @@
+function numberArgs(a,b,c,d,e) {
+	var num = arguments.length;
+	return num;
+}
+//numberArgs("a", "b", "c") ➞ 3
+//numberArgs(10, 20, 30, 40, 50) ➞ 5
+//numberArgs(x, y) ➞ 2
+//numberArgs() ➞ 0
