7a047a5471e174b8ed3883a98ebf09dfcc2d2cd7
Debraj Dey <35585013+deydebraj@users.noreply.github.com>
2019-04-17 20:22:06 -0400

    Update mapArray.js

--- mapArray.js
+++ mapArray.js
@@ -5,5 +5,14 @@ function mysteryFunc(arr, num) {
 /* Sample Inputs and Outputs
 [5, 7, 8, 2, 1], 2 ➞ [1, 1, 0, 0, 1]
 [9, 8, 16, 47], 4 ➞ [1, 0, 0, 3]
-[48, 22, 0, 19, 33, 100], 10 ➞ [8, 2, 0, 9, 3, 0]
-*/ 
+[48, 22, 0, 19, 33, 100], 10 ➞ [8, 2, 0, 9, 3, 0]*/ 
+
+
+function additiveInverse(arr) {
+	var map1 = arr.map(x => x*(-1));
+	return map1;
+}
+/* Sample Inputs and Outputs
+additiveInverse([5, -7, 8, 3]) ➞ [-5, 7, -8, -3]
+additiveInverse([1, 1, 1, 1, 1]) ➞ [-1, -1, -1, -1, -1]
+additiveInverse([-5, -25, 35]) ➞ [5, 25, -35]*/
