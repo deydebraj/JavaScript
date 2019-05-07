95000b6fef20774e964b8aec11828c4ded183bfc
Debraj Dey <35585013+deydebraj@users.noreply.github.com>
2019-05-06 23:36:58 -0400

    Update ES6ExampleOneArrayFilter.js

--- ES6ExampleOneArrayFilter.js
+++ ES6ExampleOneArrayFilter.js
@@ -1,3 +1,6 @@
+//FILTER FILTER FILTER FILTER FILTER FILTER 
+//FILTER FILTER FILTER FILTER FILTER FILTER 
+
 ages = [10,15,4,4,94,47,74,34,97,46,94,54] 
 //Find the ages which are above 21
 let temp = [];
@@ -14,3 +17,17 @@ const temp = ages.filter(function (fun){
 });
 
 const temp = ages.filter(fun => fun >= 21);
+
+
+
+const companies = [
+{name:"Seven", category:"Tech", start: 1990, end:2019},
+{name:"Two", category:"Tech", start: 1970, end:2010},
+{name:"One", category:"Oil", start: 1980, end:1983},
+{name:"Five", category:"Gas", start: 1985, end:2000}
+];
+const retailCompanies = companies.filter(fun => fun.category ==='Tech');
+
+const eightys = companies.filter(fun => (fun.start >= 1980 && fun.end < 1990));
+
+const lastedTenYears = companies.filter(fun => (fun.end - fun.start > 10));
